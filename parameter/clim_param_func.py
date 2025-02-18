8817f53410b194f640128201f2d8fb4935b3e397
cowniflow <constanze.reinken@gmx.de>
2025-02-18 17:15:26 +0100

    created synthetic test data

---
+++ parameter/clim_param_func.py
@@ -0,0 +1,18 @@
+def func_f_rate_sDist(clim):
+    return 2.8475506493010397e-11
+
+def func_f_rate_sLake(clim):
+    return 2.8475506493010397e-11
+
+def func_d_rate_sDist(clim):
+    return 1.6777300360796126e-09
+
+def func_d_rate_sLake(clim):
+    return 1.684761447714019e-09
+
+def func_mu(clim):
+    return 0.013205450108390797
+
+def func_sigma(clim):
+    return 0.07877065456847765
+
